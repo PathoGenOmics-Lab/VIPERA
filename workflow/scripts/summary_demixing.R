3dd1c7bf47561d3b76b0d8929aadfcddc5ce7cb0
Jordi Sevilla <jordisevilla2001@gmail.com>
2023-04-21 09:57:52 +0200

    Set directories as demixing summary input

--- workflow/scripts/summary_demixing.R
+++ workflow/scripts/summary_demixing.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 
 # Leer variables de snakemake ####
-names <- snakemake@params[["sample_names"]]
+directories <- snakemake@input[["directories"]]
 output_path <- snakemake@params[["demix_folder"]]
 
 # Resumir datos de freyja ####
@@ -14,15 +14,18 @@ demix <- data.frame("lineages" = NA, "abundances" = NA, "sample" = NA) %>%
   filter(!is.na(sample))
 
 # Para cada archivo extraer la info e introducirla en el df
-for (name in names){
-    path <- sprintf("%s/%s/%s_demixed.tsv", output_path, name, name)
+for (directory in directories){
+
+    COV <- str_match(directory,"COV......")
+
+    path <- sprintf("%s/%s_demixed.tsv", directory, COV)
 
     data <- read_tsv(path, col_names = c("variable", "valor")) %>%
         filter(row_number() %in% c(3, 4)) %>%
         pivot_wider(names_from = variable, values_from = valor) %>%
         separate_rows(lineages, abundances, sep = " ")
 
-    data["sample"] <- name
+    data["sample"] <- COV
 
     demix <- rbind(demix, data)
 }
