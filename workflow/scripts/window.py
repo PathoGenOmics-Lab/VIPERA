4348354b0b09f3d3421890faa1200f3ae55960c9
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2023-06-15 16:35:56 +0200

    Fix window calculation

--- workflow/scripts/window.py
+++ workflow/scripts/window.py
@@ -1,86 +1,65 @@
 #!/usr/bin/env python3
 import pandas as pd
 
-step = 1000
+
+# TODO: use GFF file for annotation
+# TODO: don't hard-code the genome length (29903)
 SCov2_annotation = {
-# "SCov2_genome"    : [1, 29903],
-"five_prime_UTR"  : [    1,   265],
-"orf1ab"          : [  266, 21555],
-"S"          : [21563, 25384],
-"ORF3a"           : [25393, 26220],
-"E"          : [26245, 26472],
-"M"          : [26523, 27191],
-"ORF6"            : [27202, 27387],
-"ORF7a"           : [27394, 27759],
-"ORF8"            : [27894, 28259],
-"N"          : [28274, 29533],
-"ORF10"           : [29558, 29674],
-"three_prime_UTR" : [29675, 29903]
+    # "SCov2_genome"    : [1, 29903],
+    "five_prime_UTR"  : [    1,   265],
+    "orf1ab"          : [  266, 21555],
+    "S"               : [21563, 25384],
+    "ORF3a"           : [25393, 26220],
+    "E"               : [26245, 26472],
+    "M"               : [26523, 27191],
+    "ORF6"            : [27202, 27387],
+    "ORF7a"           : [27394, 27759],
+    "ORF8"            : [27894, 28259],
+    "N"               : [28274, 29533],
+    "ORF10"           : [29558, 29674],
+    "three_prime_UTR" : [29675, 29903]
 }
 
-# Diccionario con relación coordenada --> gen
-coord2gene = { i : "intergenic" for i in range(1, 29903 + 1) } # Init to genomic
-for gene in SCov2_annotation:
-    start, end = SCov2_annotation[gene]
-    for i in range(start, end + 1):
-        coord2gene[i] = gene
-
-
-def cov_list(df):
-    
-    COV = list(df["REGION"].unique())
-    
-    return COV
 
 def get_polimorphic_sites(df):
-    
-    lista = set(df.POS)
-    
-    
-    return lista
+    return set(df.POS)
 
-def window_calculous(lista,step,genome_size, coord):
-    
+
+def window_calculation(sites, step, genome_size, coord):
     positions = []
     pct = []
-    genes =[] 
-    
+    genes = []
     lim_sup = genome_size + 1
-    
-    for position in range(1,lim_sup):
-
-        gene = coord[position]   
-        genes.append(gene) 
-        num_snp = 0
-
-        if position - step not in range(1,lim_sup):
-            positions.append(position)
+    for position in range(1, lim_sup):
+        # Add gene
+        gene = coord[position]
+        genes.append(gene)
+        # Add percent (excluding initial and final positions)
+        if position - step not in range(1, lim_sup):
             pct.append(0)
-             
-   
-        for x in lista:
-            if x in range(position - step,position + 1):
-                num_snp += 1
-                
-                
-        gene = 0
-            
-        frac = num_snp/1000
-        
+        else:
+            # Calculate SNPs
+            num_snp = 0
+            for x in sites:
+                if x in range(position - step, position + 1):
+                    num_snp += 1
+            pct.append(num_snp / step)
+        # Add positions
         positions.append(position)
-        pct.append(frac)
-    
-    df = {"position":positions, "fractions":pct, "gen":genes}
-    
-    df = pd.DataFrame(df)
-    
-    return df
-            
+    return pd.DataFrame({"position": positions, "fractions": pct, "gen": genes})
+
+
 def main():
+    # Diccionario con relación coordenada --> gen
+    coord2gene = {i : "intergenic" for i in range(1, 29903 + 1)}
+    for gene in SCov2_annotation:
+        start, end = SCov2_annotation[gene]
+        for i in range(start, end + 1):
+            coord2gene[i] = gene
+    # Process
     df = pd.read_table(snakemake.input.vcf)    
-    COV = cov_list(df)
     sites = get_polimorphic_sites(df)
-    frame = window_calculous(sites,step,29903,SCov2_annotation)
+    frame = window_calculation(sites, snakemake.params.step, 29903, coord2gene)
     frame.to_csv(snakemake.output.window_df)
 
 
