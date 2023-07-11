5e285fab7b5e81102516e1e755f3fe1353211a18
Jordi Sevilla <jordisevilla2001@gmail.com>
2023-07-11 12:59:50 +0200

    Documentar scripts del report

--- workflow/scripts/report/pylo.R
+++ workflow/scripts/report/pylo.R
@@ -13,15 +13,13 @@ source(snakemake@params[["design"]])
 # DATOS ####
 matrix <- read_csv(snakemake@input[["dist"]])
 metadata <- read_csv(snakemake@params[["metadata"]])
+tree_ml <- read.tree(snakemake@input[["ml"]]) %>%
+  root("NC_045512.2", resolve.root = TRUE)
+# ANÁLISIS ####
 
-# nj tree
-
-tree <- matrix %>%
-        column_to_rownames(var = "...1") %>%
-        as.matrix() %>%
-        as.dist() %>%
-        nj() %>%
-        root("NC_045512.2", resolve.root = TRUE)
+## Funciones ####
+# Con n-j puede darse que hayan ramas con longitud negativa si la matriz de distancias no es congruente
+# Esta función arregla estas distancias negativas en el caso de que ocurran
 
 fix_negative_edge_length <- function(nj.tree) {
   edge_infos <- cbind(nj.tree$edge, nj.tree$edge.length) %>% as.data.table
@@ -36,8 +34,18 @@ fix_negative_edge_length <- function(nj.tree) {
   nj.tree$edge.length <- edge_infos$length
   nj.tree
 }
+## Construcción de Arboles ####
+
+tree <- matrix %>%
+        column_to_rownames(var = "...1") %>%
+        as.matrix() %>%
+        as.dist() %>%
+        nj() %>%
+        root("NC_045512.2", resolve.root = TRUE)
 
 tree <- fix_negative_edge_length(tree)
+
+## Calculo de distancia partistica al ancestro ####
 
 tempest <- adephylo::distRoot(tree, "all", method = "patristic") %>% as.data.frame() %>% 
                     rownames_to_column(var = "ID") %>%
@@ -49,8 +57,9 @@ tempest <- adephylo::distRoot(tree, "all", method = "patristic") %>% as.data.fra
 
 
 
-# PLOTS ####
-# tree
+# FIGURAS ####
+
+# Árbol por distancias ponderadas
 tree_plot <- ggtree(tree) + 
 geom_tiplab() + 
 geom_treescale() + 
@@ -62,8 +71,7 @@ ggsave(filename = snakemake@output[["tree"]],
         dpi=250)
 
 
-# temp-est
-
+# Análisis temp-est
 tempest_fig <- ggplot(tempest) +
                 aes(x = date_interval, y = distance) + 
                 geom_smooth(method = "lm",fill = "gray95", alpha = 0.6) +
@@ -79,13 +87,10 @@ ggsave(filename = snakemake@output[["temest"]],
 
 
 
-# ML tree para el contexto ####
-
-tree <- read.tree(snakemake@input[["ml"]]) %>%
-  root("NC_045512.2", resolve.root = TRUE)
+# ML tree para el contexto 
 
 colors <- c()
-for(ID in tree$tip.label){
+for(ID in tree_ml$tip.label){
   if(ID %in% metadata$ID){
     colors <- c(colors,"red")
   } else{
@@ -93,7 +98,7 @@ for(ID in tree$tip.label){
   }
 }
 
-plot <- ggtree(tree) + 
+plot <- ggtree(tree_ml) + 
   geom_tippoint(color = colors) + 
   geom_treescale() + 
   geom_rootedge(0.00001)
