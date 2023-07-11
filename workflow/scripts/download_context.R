d10a0cb885456dc38ad0579f84f018e8b6a4a7b5
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2023-07-11 13:54:07 +0200

    Add checkpoints for context download
    - Short time windows
    - Too few available samples

--- workflow/scripts/download_context.R
+++ workflow/scripts/download_context.R
@@ -3,7 +3,12 @@
 
 library(GISAIDR)
 library(tidyverse)
+library(glue)
 
+CHKPT.ERROR.MSG <- paste(
+    "Please provide a sequence dataset through the CONTEXT_FASTA parameter",
+    "by editing the config/targets.yaml or via the command line (with --config)"
+)
 
 chunk <- function(x, chunk_length = snakemake@params[["chunk_length"]]) {
     split(x, ceiling(seq_along(x) / chunk_length))
@@ -15,18 +20,19 @@ chunk <- function(x, chunk_length = snakemake@params[["chunk_length"]]) {
 # ==== #
 
 # Read original sample metadata
-sample.metadata <- read_delim(snakemake@input[["metadata"]])
+sample.metadata <- read_delim(snakemake@input[["metadata"]], show_col_types = FALSE)
 
 # Get time windows
-# TODO: what to do with long tails
 dates <- sample.metadata %>%
     pull(snakemake@params[["date_column"]])
 min.date <- min(dates, na.rm = TRUE)
 max.date <- max(dates, na.rm = TRUE)
-print(max.date - min.date)
+print(glue("Time window: {difftime(max.date, min.date, units = 'days')} days (from {min.date} to {max.date})"))
 
-# TODO: manage edge case for same dates
-stopifnot(min.date != max.date)
+# Checkpoint: time window cannot be zero
+if (min.date == max.date) {
+    stop(glue("Time window is too short.\n{CHKPT.ERROR.MSG}"))
+}
 
 # Get locations (if there are multiple, sample from all of them)
 locations <- sample.metadata %>%
@@ -61,8 +67,15 @@ dataframes <- lapply(
 # Join results
 metadata <- bind_rows(dataframes)
 
-# TODO: clearer checkpoint (at least as many context as our dataset)
-stopifnot(nrow(metadata) > nrow(sample.metadata))
+# begin test
+index <- sample(seq_len(nrow(metadata)), nrow(sample.metadata))
+metadata <- metadata %>% slice(index)
+# end test
+
+# Checkpoint: at least as many context samples as our dataset
+if (nrow(metadata) < nrow(sample.metadata)) {
+    stop(glue("Too few available samples (n={nrow(metadata)}).\n{CHKPT.ERROR.MSG}"))
+}
 
 # Split sequences in chunks of sequences
 chunks <- chunk(metadata$accession_id)
