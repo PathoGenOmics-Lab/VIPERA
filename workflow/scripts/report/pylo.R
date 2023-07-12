31da5cf2b4835c204bdc7e8de81f61a13f9d2dc3
Jordi Sevilla <jordisevilla2001@gmail.com>
2023-07-12 22:02:54 +0200

    Give style to the report pt.1

--- workflow/scripts/report/pylo.R
+++ workflow/scripts/report/pylo.R
@@ -55,7 +55,17 @@ tempest <- adephylo::distRoot(tree, "all", method = "patristic") %>% as.data.fra
                     left_join(select(metadata, ID,CollectionDate)) %>%
                      mutate(date_interval = as.numeric(as.Date(CollectionDate) - min(as.Date(CollectionDate))))
 
+### Datos modelo lineal 
 
+model <- lm(distance ~ date_interval, data = tempest)
+
+df <- data.frame(stat = c("sub_rate","r2","pvalue"), values = c(model$coefficients[[2]],
+                                                                summary(model)$r.squared[[1]],
+                                                                cor.test(tempest$distance,tempest$date_interval)$p.value))
+
+df <- column_to_rownames(df,var = "stat")
+
+write.csv(df,snakemake@output[["stats_lm"]],row.names = F)                                                             
 
 # FIGURAS ####
 
@@ -74,8 +84,7 @@ ggsave(filename = snakemake@output[["tree"]],
 # Análisis temp-est
 tempest_fig <- ggplot(tempest) +
                 aes(x = date_interval, y = distance) + 
-                geom_smooth(method = "lm",fill = "gray95", alpha = 0.6) +
-                stat_cor(geom = "label") +
+                geom_smooth(method = "lm",fill = "gray95", alpha = 0.6, color = "red") +
                 geom_point() +
                 labs(y = "Root to tip distance", x = "Time since first sample")
 
@@ -98,10 +107,11 @@ for(ID in tree_ml$tip.label){
   }
 }
 
-plot <- ggtree(tree_ml) + 
+plot <- ggtree(tree_ml, layout = "circular") + 
   geom_tippoint(color = colors) + 
-  geom_treescale() + 
-  geom_rootedge(0.00001)
+  geom_treescale(x = 0.0008) + 
+  geom_rootedge(0.00001) + 
+  xlim(-0.0008,NA)
 
 
 ggsave(filename = snakemake@output[["tree_ml"]], 
