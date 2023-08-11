PathoGenOmics-Lab/VIPERA

commit c616fc5c1438c525ff67a6e681da28767e52ee0a

Fetch read reference IDs used for mapping from BAM

workflow/rules/context.smk
@@ -22,7 +22,7 @@ rule download_context:
         chunk_length = 3000,
         min_theoretical_combinations = config["DIVERSITY_BOOTSTRAP_REPS"]
     output:
-        fasta = temp(OUTDIR/"context"/"sequences.fasta"),
+        fasta = temp(CONTEXT_FASTA),
         metadata = temp(OUTDIR/"context"/"metadata.csv"),
         duplicate_accids = OUTDIR/"context"/"duplicate_accession_ids.txt"
     log:

config/config.yaml
@@ -2,8 +2,6 @@ OUTPUT_NAME:
   "case_study"
 ALIGNMENT_REFERENCE:
   "NC_045512.2"
-MAPPING_REFERENCES:
-  ["NC_045512.2", "MN908947.3"]
 PROBLEMATIC_VCF_URL:
   "https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/da322c32004f7b16bdaa6a8ee7fd24d56e79d9dc/problematic_sites_sarsCov2.vcf"
 TREE_MODEL:

config/targets.yaml
@@ -41,3 +41,5 @@ OUTPUT_DIRECTORY:
   "output"
 CONTEXT_FASTA:
   null
+MAPPING_REFERENCES_FASTA:
+  null

workflow/Snakefile
@@ -22,13 +22,21 @@ OUTPUT_NAME = config["OUTPUT_NAME"]
 OUTDIR = Path(config["OUTPUT_DIRECTORY"])
 OUTDIR.mkdir(parents=True, exist_ok=True)
 
-## Context
+## Set context to be fetched automatically if CONTEXT_FASTA=null
 if "CONTEXT_FASTA" not in config.keys() or config["CONTEXT_FASTA"] is None:
     CONTEXT_FASTA = OUTDIR/"context"/"sequences.fasta"
     if not Path(config["GISAID_YAML"]).is_file():
         sys.exit(f"Neither '{config['GISAID_YAML']}' or a context FASTA were found (see README.md).")
 else:
     CONTEXT_FASTA = config["CONTEXT_FASTA"]
+
+## Set mapping references to be fetched automatically if CONTEXT_FASTA=null
+if "MAPPING_REFERENCES_FASTA" not in config.keys() or config["MAPPING_REFERENCES_FASTA"] is None:
+    MAPPING_REFERENCES_FASTA = OUTDIR/"mapping_references.fasta"
+    if not Path(config["GISAID_YAML"]).is_file():
+        sys.exit(f"No FASTA with mapping references was found (see README.md).")
+else:
+    MAPPING_REFERENCES_FASTA = config["MAPPING_REFERENCES_FASTA"]
 
 ## Logging
 LOGDIR = OUTDIR / "logs"

workflow/rules/demix.smk
@@ -4,7 +4,7 @@ rule demix_preprocessing:
     shadow: "minimal"
     input:
         bam = get_input_bam,
-        ref_fasta = OUTDIR/"mapping_references.fasta"
+        ref_fasta = MAPPING_REFERENCES_FASTA
     params:
         minq = config["DEMIX"]["MIN_QUALITY"]
     output:

workflow/rules/fasta.smk
@@ -1,3 +1,22 @@
+rule read_bam_refs:
+    threads: 1
+    shadow: "minimal"
+    conda: "../envs/var_calling.yaml"
+    input:
+        iter_files("bam")
+    output:
+        temp(OUTDIR / "bam_ids.txt")
+    log:
+        LOGDIR / "read_bam_refs" / "log.txt"
+    shell:
+        """
+        for bam_file in {input:q}; do
+            samtools view -H "$bam_file" | grep ^@SQ | cut -d"\t" -f2 | sed 's/SN://g' >> ids.txt 2>> {log}
+        done
+        sort ids.txt | uniq > {output}
+        """
+
+
 rule rename_fastas:
     input:
         fasta = get_input_fasta

workflow/rules/fetch.smk
@@ -14,18 +14,15 @@ rule fetch_alignment_reference:
 rule fetch_mapping_references:
     threads: 1
     conda: "../envs/fetch.yaml"
-    params:
-        refs = config["MAPPING_REFERENCES"]
+    input:
+        OUTDIR / "bam_ids.txt"
     output:
-        fasta = OUTDIR/"mapping_references.fasta"
+        fasta = MAPPING_REFERENCES_FASTA
     log:
         LOGDIR / "fetch_mapping_references" / "log.txt"
     shell:
         """
-        # Create empty file
-        echo -n > {output.fasta}
-        # Download and concatenate each reference
-        echo {params.refs} | while read -r ref_id; do
+        cat {input} | while read ref_id || [[ -n $ref_id ]]; do
             esearch -db nucleotide -query "$ref_id" | efetch -format fasta >> {output.fasta} 2>> {log}
         done
         """

commit 232ea3db71056ac73eef27ea14c01da165d1a773

Fix context download path error
Fixes error introduced in c616fc5

workflow/rules/context.smk
@@ -22,7 +22,7 @@ rule download_context:
         chunk_length = 3000,
         min_theoretical_combinations = config["DIVERSITY_BOOTSTRAP_REPS"]
     output:
-        fasta = temp(CONTEXT_FASTA),
+        fasta = temp(OUTDIR/"context"/"sequences.fasta"),
         metadata = temp(OUTDIR/"context"/"metadata.csv"),
         duplicate_accids = OUTDIR/"context"/"duplicate_accession_ids.txt"
     log: