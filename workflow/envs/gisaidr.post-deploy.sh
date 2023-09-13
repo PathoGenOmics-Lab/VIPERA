f41ca3cc51762eb9f62f89626d5e2464682376e3
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2023-09-13 16:21:31 +0200

    Add GISAIDR version

--- workflow/envs/gisaidr.post-deploy.sh
+++ workflow/envs/gisaidr.post-deploy.sh
@@ -2,7 +2,7 @@
 
 # Install GISAIDR
 # https://github.com/Wytamma/GISAIDR
-Rscript -e 'devtools::install_github("Wytamma/GISAIDR")'
+Rscript -e 'devtools::install_github("Wytamma/GISAIDR", ref = "v0.9.9")'
 
 # Install curl
 Rscript -e 'install.packages("curl", repos = "https://cloud.r-project.org/")'
