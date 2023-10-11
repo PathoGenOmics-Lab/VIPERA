409c1c8a250442ac40e18f5ecf09fefa2a8e4d06
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2023-10-11 11:55:59 +0200

    Remove hard-coded genome length

--- workflow/scripts/window.py
+++ workflow/scripts/window.py
@@ -6,16 +6,12 @@ import pandas as pd
 from gb2seq.features import Features
 
 
-def get_polimorphic_sites(df: pd.DataFrame) -> set:
-    return set(df.POS)
-
-
-def window_calculation(sites: set, window: int, step: int, genome_size: int, coord: str) -> pd.DataFrame:
+def window_calculation(sites: set, window: int, step: int, coord: str) -> pd.DataFrame:
     ft = Features(coord) # Create Features object to obtain annotations
     positions = []
     pct = []
     genes = []
-    lim_sup = genome_size + 1
+    lim_sup = len(ft.reference.sequence) + 1
     for position in range(1, lim_sup, step):
         if len(ft.getFeatureNames(position)) == 0:
             genes.append("Intergenic")
@@ -25,7 +21,7 @@ def window_calculation(sites: set, window: int, step: int, genome_size: int, coo
         if position - window not in range(1, lim_sup):
             pct.append(0)
         else:
-            # Calculate nº of polimorphisms in the window
+            # Calculate no. of polimorphisms in the window
             num_snp = len([x for x in sites if x in range(position - window, position + 1)])
             pct.append(num_snp / window)
         positions.append(position)
@@ -38,17 +34,14 @@ def main():
 
     logging.info("Reading input VCF")
     df = pd.read_table(snakemake.input.vcf)
+    sites = set(df.POS)
 
     logging.info("Reading features")
     with open(snakemake.input.features) as f:
         features_key = json.load(f)
 
-    logging.info("Getting polimorphic sites")
-    sites = get_polimorphic_sites(df)
-
     logging.info("Sliding window calculation of proportion of polimorphic sites")
-
-    frame = window_calculation(sites, snakemake.params.window, snakemake.params.step, 29903, snakemake.input.gb)
+    frame = window_calculation(sites, snakemake.params.window, snakemake.params.step, snakemake.input.gb)
     
     logging.info("Saving results")
     frame.replace(features_key, inplace = True)
