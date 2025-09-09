ac3512aabdb65b22ab6777e7c46f4b7b241b570e
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2025-09-09 12:43:14 +0200

    Specify column types

--- workflow/scripts/merge_annotation.R
+++ workflow/scripts/merge_annotation.R
@@ -10,13 +10,28 @@ library(logger)
 log_threshold(INFO)
 
 log_info("Reading variants table, replacing REGION with the reference name")
-variants <- read_tsv(snakemake@input$tsv) %>%
+variants <- read_tsv(
+  snakemake@input$tsv,
+  col_types = list(
+    CHROM = col_character(),
+    POS = col_integer(),
+    REF = col_character(),
+    ALT = col_character()
+  )
+) %>%
   mutate(REGION = snakemake@params$ref_name)
 
 log_info("Reading annotation table")
 annotation <- read_tsv(
   snakemake@input$annot,
-  col_select = c("CHROM", "POS", "REF", "ALT", "VARIANT_NAME")
+  col_select = c("CHROM", "POS", "REF", "ALT", "VARIANT_NAME"),
+  col_types = list(
+    CHROM = col_character(),
+    POS = col_integer(),
+    REF = col_character(),
+    ALT = col_character(),
+    VARIANT_NAME = col_character()
+  )
 ) %>%
   distinct() %>%
   group_by(CHROM, POS, REF, ALT) %>%
