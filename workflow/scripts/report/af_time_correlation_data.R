PathoGenOmics-Lab/VIPERA

commit c12cf2a481f55212cbfe0cf46fdccecb6c34ca50

refactor: add AF amplitude threshold to AF time correlation rule

workflow/scripts/report/af_time_correlation_data.R
@@ -95,6 +95,8 @@ correlations <- lapply(
     add_row(
       cor.df,
       variant = snp,
+      min_af = min(df$ALT_FREQ, na.rm = TRUE),
+      max_af = max(df$ALT_FREQ, na.rm = TRUE),
       coefficient = test$estimate,
       p.value = test$p.value,
       p.value.adj = p.value.adj
@@ -113,7 +115,8 @@ log_info("Selecting variants whose allele frequency is significantly correlated 
 significant.variants <- correlations %>%
   filter(
     p.value.adj <= snakemake@params$max_p_adj_threshold,
-    abs(coefficient) >= snakemake@params$min_abs_cor_threshold
+    abs(coefficient) >= snakemake@params$min_abs_cor_threshold,
+    (max_af - min_af) >= snakemake@params$min_diff_af_threshold
   ) %>%
   pull(variant) %>%
   unique()

workflow/rules/vaf.smk
@@ -288,6 +288,7 @@ rule af_time_correlation_data:
         cor_exact=config["COR"]["EXACT"],
         max_p_adj_threshold = 0.05,
         min_abs_cor_threshold = 0.0,
+        min_diff_af_threshold = 0.0,
     input:
         variants=OUTDIR / f"{OUTPUT_NAME}.variants.all_sites.tsv",
         metadata=config["METADATA"],

commit 52529f331a924ee64f0ff281ebf5eac4db3ede57

fix: init missing columns
Fixes error introduced in c12cf2a48

workflow/scripts/report/af_time_correlation_data.R
@@ -63,6 +63,8 @@ unique.snps <- unique(variants$VARIANT_NAME)
 # Create an empty dataframe to be filled
 cor.df <- data.frame(
   variant = "",
+  min_af = 0,
+  max_af = 0,
   coefficient = 0,
   p.value = 0,
   p.value.adj = 0