PathoGenOmics-Lab/VIPERA

commit 8bc79b061b43c10cd7b3db97afee537ac8661b01

fix: handle variants with fewer than necessary time points

workflow/scripts/report/af_time_correlation_data.R
@@ -48,7 +48,11 @@ variants <- left_join(variants, metadata, by = c("SAMPLE" = "ID")) %>%
     CollectionDate
   ) %>%
   mutate(
-    interval = as.numeric(difftime(CollectionDate, min(CollectionDate), units = "days"))
+    interval = as.numeric(difftime(
+      CollectionDate,
+      min(CollectionDate),
+      units = "days"
+    ))
   )
 
 # Save processed input
@@ -81,12 +85,21 @@ correlations <- lapply(
       VARIANT_NAME == snp
     )
     # Perform calculation
-    test <- cor.test(
-      df$ALT_FREQ,
-      df$interval,
-      method = snakemake@params$cor_method,
-      exact = snakemake@params$cor_exact
+    test <- tryCatch(
+      {
+        cor.test(
+          df$ALT_FREQ,
+          df$interval,
+          method = "spearman",
+          exact = TRUE
+        )
+      },
+      error = function(e) {
+        log_warn("Cannot run cor.test for {snp}: {e}")
+        list(p.value = NA, estimate = NA)
+      }
     )
+
     # Adjust p-value
     p.value.adj <- p.adjust(
       test$p.value,

commit ea031ed4dce27ea5325251180a7f623ea3f3c9c7

fix: add back snakemake option
reverts bug introduced in 8bc79b0

workflow/scripts/report/af_time_correlation_data.R
@@ -90,8 +90,8 @@ correlations <- lapply(
         cor.test(
           df$ALT_FREQ,
           df$interval,
-          method = "spearman",
-          exact = TRUE
+          method = snakemake@params$cor_method,
+          exact = snakemake@params$cor_exact
         )
       },
       error = function(e) {