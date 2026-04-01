806107e280814eda4e9db8147597183014d1aeff
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2026-04-01 12:08:08 +0200

    refactor: simplify all-sites filling

--- workflow/scripts/fill_all_sites.R
+++ workflow/scripts/fill_all_sites.R
@@ -1,6 +1,4 @@
 #!/usr/bin/env Rscript
-
-# Write stdout and stderr to log file
 log <- file(snakemake@log[[1]], open = "wt")
 sink(log, type = "message")
 sink(log, type = "output")
@@ -15,11 +13,6 @@ log_threshold(INFO)
 log_info("Reading variants")
 variants <- read_tsv(snakemake@input[["variants"]])
 
-# Create a mapping of variant names to their genomic position
-variant_coords <- variants %>%
-  select(VARIANT_NAME, CHROM, POS) %>%
-  distinct()
-
 log_info("Reading filtered sites")
 sites <- read_tsv(snakemake@input[["sites"]]) %>%
   distinct(SAMPLE, POS) %>%  # TODO: consider region/chrom
@@ -28,14 +21,9 @@ sites <- read_tsv(snakemake@input[["sites"]]) %>%
 log_info("Processing variants")
 all_variants <- variants %>%
   # Select minimal columns
-  distinct(VARIANT_NAME, CHROM, SAMPLE, ALT_FREQ) %>%
-  # Handle duplicates
-  group_by(SAMPLE, VARIANT_NAME, CHROM) %>%
-  summarise(ALT_FREQ = sum(ALT_FREQ, na.rm = TRUE), .groups = "drop") %>%
+  distinct(VARIANT_NAME, CHROM, POS, SAMPLE, ALT_FREQ) %>%
   # Complete with NA
-  complete(SAMPLE, VARIANT_NAME, CHROM) %>%
-  # Assign genomic positions for all combinations
-  left_join(variant_coords, by = c("CHROM", "VARIANT_NAME")) %>%
+  complete(SAMPLE, nesting(VARIANT_NAME, CHROM, POS)) %>%
   # Merge filtered sites
   # TODO: consider region/chrom
   left_join(sites, by = c("SAMPLE", "POS")) %>%
