76b29578710a9f75a443f2665605eda73a12519b
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2026-05-03 00:36:02 +0200

    fix: apply negative branch length correction iteratively (#45)

--- workflow/scripts/report/allele_freq_tree_data.R
+++ workflow/scripts/report/allele_freq_tree_data.R
@@ -19,11 +19,13 @@ fix_negative_edge_length <- function(nj.tree) {
   ) %>%
     as.data.table()
   colnames(edge_infos) <- c("from", "to", "length")
-  nega_froms <- edge_infos[length < 0, sort(unique(from))]
-  for (nega_from in nega_froms) {
-    minus_length <- edge_infos[from == nega_from, ][order(length)][1, length]
-    edge_infos[from == nega_from, length := length - minus_length]
-    edge_infos[to == nega_from, length := length + minus_length]
+  while (any(edge_infos$length < 0)) {
+    nega_froms <- edge_infos[length < 0, sort(unique(from))]
+    for (nega_from in nega_froms) {
+      minus_length <- edge_infos[from == nega_from, ][order(length)][1, length]
+      edge_infos[from == nega_from, length := length - minus_length]
+      edge_infos[to == nega_from, length := length + minus_length]
+    }
   }
   nj.tree$edge.length <- edge_infos$length
   nj.tree
